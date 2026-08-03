a3a640e854b50ab724b2f1c0666a7995f0d34af4
Kulikov Aleksey <k.alex.serg@gmail.com>
2026-08-04 00:50:31 +0200

    Run interface controller root regression test

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -24,6 +24,7 @@ perl tests/async-js-test.pl
 perl tests/api-json-test.pl
 perl tests/miniserv-json-test.pl
 perl tests/peer-list-actions-test.pl
+perl tests/interface-controller-root-test.pl
 perl tests/peer-runtime-refresh-test.pl
 perl tests/diagnostic-js-test.pl
 perl tests/security-regression-test.pl
