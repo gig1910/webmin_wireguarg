7c27c92ce41d2179235cc1bb3aaf8949f73aea46
Kulikov Aleksey <k.alex.serg@gmail.com>
2026-08-04 00:36:44 +0200

    Test runtime cache invalidation after peer CRUD

--- tests/peer-runtime-refresh-test.pl
+++ tests/peer-runtime-refresh-test.pl
@@ -2,50 +2,18 @@
 use strict;
 use warnings;
 use FindBin;
-
-sub slurp {
-    my ($path) = @_;
-    open(my $fh, '<', $path) or die "$path: $!\n";
-    local $/;
-    my $data = <$fh>;
-    close($fh);
-    return $data;
+sub slurp { my ($p)=@_; open(my $f,'<',$p) or die "$p: $!\n"; local $/; my $d=<$f>; close($f); return $d; }
+for my $file (qw(toggle_peer.cgi save_peer.cgi delete_peer.cgi)) {
+    my $data = slurp("$FindBin::Bin/../$file");
+    die "$file does not invalidate runtime cache\n" if $data !~ /runtime_snapshot_path\(\)/ || $data !~ /unlink\(\$runtime_path\)/;
 }
-
-my $toggle = slurp("$FindBin::Bin/../toggle_peer.cgi");
-die "peer toggle does not refresh the shared runtime snapshot\n"
-    if $toggle !~ /refresh_runtime_snapshot\(0\)/;
-die "peer toggle refresh happens before runtime apply\n"
-    if index($toggle, 'refresh_runtime_snapshot(0)') < index($toggle, 'action_apply_interface');
-die "peer toggle JSON does not report runtime refresh state\n"
-    if $toggle !~ /runtime_refreshed/;
-
-my $save = slurp("$FindBin::Bin/../save_peer.cgi");
-die "peer create does not refresh runtime snapshot\n"
-    if $save !~ /refresh_runtime_snapshot\(0\)/;
-die "new peer does not return directly to the interface table\n"
-    if $save !~ /\$is_new\s*&&\s*\$apply_ok[\s\S]+redirect\('edit_interface\.cgi\?name='/;
-die "peer create redirects before refreshing runtime\n"
-    if index($save, 'redirect(') < index($save, 'refresh_runtime_snapshot(0)');
-
-my $delete = slurp("$FindBin::Bin/../delete_peer.cgi");
-die "peer delete does not refresh runtime snapshot\n"
-    if $delete !~ /refresh_runtime_snapshot\(0\)/;
-die "peer delete does not return directly to the interface table\n"
-    if $delete !~ /redirect\('edit_interface\.cgi\?name='/;
-die "peer delete redirects before refreshing runtime\n"
-    if index($delete, 'redirect(') < index($delete, 'refresh_runtime_snapshot(0)');
-
-my $runtime = slurp("$FindBin::Bin/../runtime.cgi");
-die "runtime endpoint lacks an explicit force-refresh mode\n"
-    if $runtime !~ /force_refresh/ || $runtime !~ /refresh_runtime_snapshot\(\$force_refresh \? 0/;
-die "forced runtime requests are not rate limited\n"
-    if $runtime !~ /request_rate_limit\('runtime_refresh'/;
-
-my $metrics = slurp("$FindBin::Bin/../lib/metrics-lib.pl");
-die "runtime poller exposes no refresh method\n"
-    if $metrics !~ /state\.refresh=function/;
-die "runtime poller does not support refresh=1\n"
-    if $metrics !~ /refresh=1&_wg_now=/;
-
-print "peer CRUD runtime refresh tests passed\n";
+my $toggle=slurp("$FindBin::Bin/../toggle_peer.cgi");
+die "toggle invalidates cache before apply\n" if index($toggle,'unlink($runtime_path)') < index($toggle,'action_apply_interface');
+die "toggle response lacks invalidation status\n" if $toggle !~ /runtime_cache_invalidated/;
+my $save=slurp("$FindBin::Bin/../save_peer.cgi");
+die "new peer does not return to table\n" if $save !~ /\$is_new\s*&&\s*\$apply_ok[\s\S]+redirect\('edit_interface\.cgi\?name='/;
+die "save redirects before cache invalidation\n" if index($save,'redirect(') < index($save,'unlink($runtime_path)');
+my $delete=slurp("$FindBin::Bin/../delete_peer.cgi");
+die "delete does not return to table\n" if $delete !~ /redirect\('edit_interface\.cgi\?name='/;
+die "delete redirects before cache invalidation\n" if index($delete,'redirect(') < index($delete,'unlink($runtime_path)');
+print "peer CRUD runtime cache invalidation tests passed\n";
