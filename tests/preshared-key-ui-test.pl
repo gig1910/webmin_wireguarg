6cad7179e69c1e8dd84d3cde1e08427b3b33ada4
Kulikov Aleksey <k.alex.serg@gmail.com>
2026-08-04 02:31:52 +0200

    Test mutually exclusive PresharedKey UI states

---
+++ tests/preshared-key-ui-test.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/stub";
+use lib "$FindBin::Bin/..";
+our (%config, %text, %in, $WG_PSK_ORIGINAL_UI_TABLE_ROW);
+$config{'wg_cmd'} = "$FindBin::Bin/bin/wg";
+require "$FindBin::Bin/../wireguard-lib.pl";
+
+local $WG_PSK_ORIGINAL_UI_TABLE_ROW = sub {
+    my ($label, $value) = @_;
+    return '<tr><th>'.$label.'</th><td>'.$value.'</td></tr>';
+};
+
+{
+    no warnings qw(redefine once);
+    local *main::ui_checkbox = sub {
+        my ($name, $value, $label) = @_;
+        return '<label><input type="checkbox" name="'.$name.'" value="'.$value.'">'.$label.'</label>';
+    };
+
+    my $configured = preshared_key_form_rows_for_state(1);
+    die "configured PresharedKey notice missing\n"
+        if ($configured !~ /alert-success/ || $configured !~ /PresharedKey/);
+    die "configured PresharedKey remove button missing\n"
+        if ($configured !~ /type="submit"/ || $configured !~ /name="remove_preshared_key"/);
+    die "configured PresharedKey still shows creation controls\n"
+        if ($configured =~ /name="preshared_key"/ || $configured =~ /generate_preshared_key/);
+
+    my $missing = preshared_key_form_rows_for_state(0);
+    die "missing PresharedKey input not shown\n"
+        if ($missing !~ /type="password"/ || $missing !~ /name="preshared_key"/);
+    die "missing PresharedKey generation option not shown\n"
+        if ($missing !~ /name="generate_preshared_key"/);
+    die "missing PresharedKey incorrectly shows remove action\n"
+        if ($missing =~ /name="remove_preshared_key"/);
+}
+
+print "PresharedKey UI state tests passed\n";
